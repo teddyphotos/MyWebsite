89ef305705ce5b9c6ccd10ff5ea43667c012d63c
teddyphotos <goelkashish4@gmail.com>
2020-02-19 22:04:32 +0530

    Added basic layout

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+
+
+window.addEventListener('scroll',(e)=>{
+
+  
+  
+        
+    if(window.pageYOffset>10){
+      // When scrolled down
+      // document.getElementById("navbar-main").style.boxShadow = "0px 0px 10px #9e9e9e";
+      // document.getElementById("navbar-main").style.top = "-75px";
+
+    }else{
+      // When at top of page
+      // document.getElementById("navbar-main").style.boxShadow = "";
+      // document.getElementById("navbar-main").style.top = "0";
+
+    }
+  });
